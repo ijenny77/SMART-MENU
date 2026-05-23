bc4b8abc956df4e68bd5e34b66b3922d9233050a
unknown <ishimwejennymiriotta@gmail.com>
2026-05-23 22:32:06 +0200

    Modification of the login page.

--- src/components/pages/marketing/LoginPage.jsx
+++ src/components/pages/marketing/LoginPage.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
 import LoginForm from './LoginForm'
+import styles from './LoginPage.module.css'
+import clipboard from '../../../assets/Group 52.png'
+import busdelivery from '../../../assets/Group 50.png'
+import lamp from '../../../assets/Group 53.png'
+import apple from '../../../assets/Group 46.png'
 
+const items = [
+  {
+    icon:clipboard,
+    heading:'Digital Menu Management ',
+    paragraph:'Create, update and organize your menu items in just a few clicks.'
+  },
+  {
+    icon:busdelivery,
+    heading:'Real-time Orders ',
+    paragraph:'Receive and manage customer orders in real time.'
+  },
+  {
+    icon:lamp,
+    heading:'Smart Insights ',
+    paragraph:'Track performance and understand your business better.'
+  }
+]
 const LoginPage = () => {
   return (
-    <div>
-      <LoginForm/>
-    </div>
+      <div className={styles.mainLoginPage}>
+        <div className={styles.info}>
+          <p className={styles.quote}>EASY . FAST . SMART</p>
+          <p className={styles.text}>Manage Your Restaurant <br/> Like Never Before </p>
+          <div className={styles.leftside}>
+            <div>
+              {items.map((item) => (
+                <div className={styles.boxes}>
+                  <img src={item.icon} alt="" />
+                  <div className={styles.grpText}>
+                    <p className={styles.heading}>{item.heading}</p>
+                    <p className={styles.paragraph}>{item.paragraph}</p>
+                  </div>
+                </div>
+                ))}
+            </div>
+            <img className={styles.apple} src={apple} alt="" />
+          </div>
+        </div>
+        <LoginForm/>
+      </div>
   )
 }
 
--- src/components/pages/marketing/loginForm.jsx
+++ src/components/pages/marketing/loginForm.jsx
@@ -3,8 +3,8 @@ import Input from '../../ui/Input'
 import styles from  './LoginForm.module.css'
 import {Link} from 'react-router-dom'
 import Button from '../../ui/Button'
-import email from '../../../assets/email 2.png'
-import lock from '../../../assets/lock 1.png'
+// import email from '../../../assets/email 2.png'
+// import lock from '../../../assets/lock 1.png'
 
 const loginForm = () => {
   return (
@@ -12,10 +12,10 @@ const loginForm = () => {
         <p className={styles.welcome}>Welcome</p>
         <p className={styles.loginSM}>Login to SMART MENU</p>
         <label style={{marginBottom:'0.7rem'}}>Email</label>
-        <Input type="email" className={styles.email} placeholder={email}/>
+        <Input type="email" className={styles.email} />
         <label>Password</label>
         <p className={styles.forgotpass}>Forgot password?</p>
-        <Input type="password" className={styles.password} placeholder={lock}/>
+        <Input type="password" className={styles.password}/>
         <Button className={styles.login}>Log in </Button>
         <span className={styles.noAcc}>Don't have an account?<Link className={styles.signup}>Sign up?</Link></span>
     </div>
