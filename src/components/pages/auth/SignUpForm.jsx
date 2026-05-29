56d93defa66683a897d0923e2eaa104808a83ae3
unknown <ishimwejennymiriotta@gmail.com>
2026-05-29 15:31:16 +0200

    feat(auth): add sign up page

--- src/components/pages/auth/SignUpForm.jsx
+++ src/components/pages/auth/SignUpForm.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import Input from '../../ui/Input'
-
+import styles from './SignUpForm.module.css'
+import Button from '../../ui/Button'
+import Twitter from '../../../assets/Twitter.png'
+import Google from '../../../assets/google-color 1.png'
+import Apple from '../../../assets/apple-173 1.png'
+import Facebook from '../../../assets/Group 37.png'
 const SignUpForm = () => {
   return (
-    <div>
-        <p>Get started</p>
-        <Input/>
+    <div className={styles.mainSignUpForm}>
+        <p className={styles.started}>Get started</p>
+        <label>Full Name</label>
+        <Input type="text" placeholder="Enter Full name"/>
+        <label>Email</label>
+        <Input type="email" placeholder="Enter Email"/>
+        <label>Password</label>
+        <Input type="password" placeholder="Enter Password"/>
+        <div className={styles.checkingbox}>
+            <Input className={styles.checkbox} type="checkbox" placeholder=""/>
+            <p className={styles.agreeTermso }>I agree to the processing of <span style={{fontWeight:"900"}}>Personal data</span></p>
+        </div>
+        <Button className={styles.signUp}>Sign up</Button>
+        <span className={styles.divider}>Sign up with</span>
+        <div className={styles.icons}>
+            <a href="#"><img src={Facebook} alt="" /></a>
+            <a href="#"><img src={Twitter} alt="" /></a>
+            <a href="#"><img src={Google} alt="" /></a>
+            <a href="#"><img src={Apple} alt="" /></a>
+        </div>
+        <p className={styles.haveAcc}>Already have account? <a href="#" className={styles.signIn}>Sign in</a></p>
     </div>
   )
 }
--- src/components/pages/auth/SignUpPage.jsx
+++ src/components/pages/auth/SignUpPage.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import styles from './SignUpPage.module.css'
-import SignUpForm from './SignUPForm'
+import SignUpForm from './SignUpForm'
+import coffeeCombo from '../../../assets/coffeeCombo.png'
+import time from '../../../assets/Group 116.png'
+import tracks from '../../../assets/Group 118.png'
+import chairs from '../../../assets/Group 121.png'
 const SignUpPage = () => {
   return (
     <div className={styles.mainSignUpPage}>
+      <div className={styles.textSide}>
+        <p className={styles.welcomeSmartMenu}>Welcome to <span style={{color:"#CEFBE0"}}>SMART MENU</span></p>
+        <p className={styles.manageRestaurant}>Manage your restaurant smarter faster</p>
+        <div className={styles.card1}>
+          <img className={styles.icons} src={chairs} alt="" />
+          <div>
+            <p className={styles.headings}>Easy Menu Management</p>
+            <p className={styles.paragraph}>Create and update menus in just a few clicks.</p>
+          </div>
+        </div>
+        <div className={styles.card2}>
+          <img className={styles.icons} src={tracks} alt="" />
+          <div>
+            <p className={styles.headings}>Track Performance</p>
+            <p className={styles.paragraph}>Great real-time insights and grow your business.</p>
+          </div>
+        </div>
+        <div className={styles.card3}>
+          <img className={styles.icons} src={time} alt="" />
+          <div>
+            <p className={styles.headings}>Save Time</p>
+            <p className={styles.paragraph}>Automate tasks and focus on what matters most.</p>
+          </div>
+        </div>
+      </div>
+      <img className={styles.coffeeCombo} src={coffeeCombo} alt="" />
       <SignUpForm/>
     </div>
   )
